3a9f4ecf4978989a9cd071394568a104c154d9d2
Charlie D <charlieding21@gmail.com>
2021-11-30 16:30:53 +1100

    feat: skeleton navbar design + home page

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import React from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router} from 'react-router-dom'
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Switch>
+          <Route path='/' exact component={Home} />
+      </Switch>
     </Router>
   );
 }
